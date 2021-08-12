d35a858d49f75b57603ad91ac481fab05e237c69
ThaddG <thaddious.gorges@gmail.com>
2021-08-12 18:45:11 -0400

    max width on header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,30 +13,32 @@ export default function Header() {
   return (
     <Container maxWidth={false} className="container-override pt-2">
       <Navigation />
-      <Grid
-        className="header-text"
-        container
-        alignContent="center"
-      >
-        <Grid item>
-          <Paper elevation={10} className="px-1 py-4">
-            <Container maxWidth="sm">
-              <Typography variant="h4" style={{ fontWeight: 'bold' }}>
-                Maximize your rewards with credit cards that compliment one
-                another
-              </Typography>
-              <Typography variant="subtitle1">
-                Increase your income by stacking credit cards with benefits that
-                compliment each other
-              </Typography>
-              <Button className="mt-3 mb-2" variant="contained" color="primary">
-                View Stacks
-                <ArrowForwardIcon></ArrowForwardIcon>
-              </Button>
-            </Container>
-          </Paper>
+      <div style={{ maxWidth: '1300px', margin: '0 auto' }}>
+        <Grid className="header-text" container alignContent="center">
+          <Grid item>
+            <Paper elevation={10} className="px-1 py-4">
+              <Container maxWidth="sm">
+                <Typography variant="h4" style={{ fontWeight: 'bold' }}>
+                  Maximize your rewards with credit cards that compliment one
+                  another
+                </Typography>
+                <Typography variant="subtitle1">
+                  Increase your income by stacking credit cards with benefits
+                  that compliment each other
+                </Typography>
+                <Button
+                  className="mt-3 mb-2"
+                  variant="contained"
+                  color="primary"
+                >
+                  View Stacks
+                  <ArrowForwardIcon></ArrowForwardIcon>
+                </Button>
+              </Container>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </Container>
   );
 }
