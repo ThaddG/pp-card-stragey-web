3107d03334a3831258c3015ac6562922490e6c1d
ThaddG <thaddious.gorges@gmail.com>
2021-08-24 20:50:44 -0400

    add backbutton to dashboard

--- src/pages/CMS/Dashboard.tsx
+++ src/pages/CMS/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Button } from '@material-ui/core';
 
@@ -6,10 +6,12 @@ import { Typography, Button } from '@material-ui/core';
 import CardsList from './CardsList';
 import AddCard from './AddCard';
 import EditCard from './EditCard';
+import BackButton from '../../components/BackButton';
 
 export default function Dashbaord() {
   return (
-    <>
+    <div style={{width: '350px', margin: '60px auto 0'}}>
+      <BackButton text="home" to="/" />
       <Typography variant="h2">Dashboard</Typography>
       <Link to="/cms/list">
         <Button
@@ -33,6 +35,6 @@ export default function Dashbaord() {
           Add Card
         </Button>
       </Link>
-    </>
+    </div>
   );
 }
