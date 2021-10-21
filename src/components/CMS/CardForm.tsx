605c1d648cf5fe464a99fa2c77520190b1aac4bf
ThaddG <thaddious.gorges@gmail.com>
2021-10-21 03:09:54 -0400

    removed unused type

--- src/components/CMS/CardForm.tsx
+++ src/components/CMS/CardForm.tsx
@@ -28,7 +28,6 @@ import '../../styles/components/CMS/CardForm.css';
 
 // types
 import { RewardTypeProps, RewardType, RewardTypeValues } from '../../types';
-type BusinessOrPersonal = 'business' | 'personal';
 
 interface Props {
   title: string;
