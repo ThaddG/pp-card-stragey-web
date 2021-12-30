b6d2060eac9f0895d7422b83d70a0c11a6299e79
ThaddG <thaddious.gorges@gmail.com>
2021-12-30 00:25:33 -0500

    added temporary stack list item

--- src/pages/CMS/StackList.tsx
+++ src/pages/CMS/StackList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box, Typography } from '@material-ui/core';
 import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
 
 // redux
@@ -6,6 +7,9 @@ import {
   useAppDispatch as useDispatch,
   useAppSelector as useSelector,
 } from '../../hooks';
+
+// styles
+import { StyledBox } from '../../styles/components/CMS/StackListItem.styles';
 
 // types
 import StackProps from '../../models/stack';
@@ -19,7 +23,16 @@ export default function StackList() {
         ? 'Loading stacks'
         : isEmpty(stacks)
         ? 'There are no cards'
-        : stacks.map((stack: StackProps) => <p>{stack.title}</p>)}
+        : stacks.map((stack: StackProps) => <StackListItem stack={stack} />)}
     </>
-  )
+  );
 }
+
+type StackItemProps = {
+  stack: StackProps;
+};
+const StackListItem: React.FC<StackItemProps> = ({ stack }) => (
+  <StyledBox sx={{ width: 300, height: 300, color: '#fff', m: 1 }}>
+    <Typography>{stack.title}</Typography>
+  </StyledBox>
+);
---
+++ src/styles/components/CMS/StackListItem.styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Box } from '@material-ui/core';
+
+export const StyledBox = styled(Box)`
+  background-color: tomato;
+  cursor: pointer;
+`;
