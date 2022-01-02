584c33abfb603ba43b3fe8422448ef872172e204
ThaddG <thaddious.gorges@gmail.com>
2022-01-01 23:10:05 -0500

    removed unused code

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import Typography from '@mui/material/Typography'
 import {ReactComponent as PPLogo} from '../assets/plentyPayLogo-02 1.svg'
 
 const Logo = () => {
 
   return (
-    <div style={{backgroundColor: 'red', width: 200, height: 120}}>
+    <div>
       <PPLogo />
     </div>
   )
