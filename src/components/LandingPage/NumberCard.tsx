d2e3281a4c432899446e51c65db2d01dc9eaa8cb
ThaddG <thaddious.gorges@gmail.com>
2022-01-03 03:29:29 -0500

    added icons to steps

--- src/components/LandingPage/NumberCard.tsx
+++ src/components/LandingPage/NumberCard.tsx
@@ -1,5 +1,6 @@
 import Typography from '@mui/material/Typography';
 import { Container as MuiContainer } from '@mui/material';
+import { ReactComponent as StepIcon } from '../../assets/icons/step-icon.svg';
 
 // styles
 import { Container, Number } from '../../styles/LandingPage/NumberCard.styles';
@@ -15,8 +16,9 @@ const NumberCard: React.FC<Props> = ({ number, title, bodyText }) => {
   return (
     <Container sx={{ position: 'relative' }}>
       <Number>{number}</Number>
+      <StepIcon className="mb-3" />
       <MuiContainer>
-        <Typography variant="h5" sx={{ textAlign: 'center' }}>
+        <Typography variant="h5" sx={{ textAlign: 'center', mb: 1 }}>
           {title}
         </Typography>
         <Typography variant="body1">{bodyText}</Typography>
