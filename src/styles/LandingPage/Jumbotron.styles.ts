f8d7648cb343a45fea74691eefd40041b691819f
ThaddG <thaddious.gorges@gmail.com>
2022-01-03 23:16:02 -0500

    fixed jumbotron overflow

--- src/styles/LandingPage/Jumbotron.styles.ts
+++ src/styles/LandingPage/Jumbotron.styles.ts
@@ -5,9 +5,9 @@ interface Props {
 }
 
 export const Container = styled.div<Props>`
-  width: 100vw;
-  height: 620px;
-  padding: 0.5rem;
+  width: 100%;
+  height: 625px;
+  padding: 0.5rem 0;
   background-image: url(${(props) => props.image});
   background-size: cover;
   background-position: center;
