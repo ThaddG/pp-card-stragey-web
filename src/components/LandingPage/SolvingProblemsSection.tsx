7f84172a25eba97b9be8384219f9ac23a19c4ea9
ThaddG <thaddious.gorges@gmail.com>
2022-01-12 23:16:38 -0500

    added section header

--- src/components/LandingPage/SolvingProblemsSection.tsx
+++ src/components/LandingPage/SolvingProblemsSection.tsx
@@ -1,19 +1,23 @@
-import {Container as MuiContainer, Typography} from '@mui/material';
+import { Container as MuiContainer, Typography } from '@mui/material';
 
 // styles
-import {Container} from '../../styles/LandingPage/global.styles'
+import { Container } from '../../styles/LandingPage/global.styles';
 
 const SolvingProblemsSection = () => {
   return (
     <Container backgroundColor="#fff">
       <MuiContainer>
         {/* TODO: change font to Poppins */}
-        <Typography variant="body2">
+        <Typography variant="body2" sx={{ mb: 2 }}>
           SOLVING PROBLEMS
+        </Typography>
+        <Typography variant="h5">
+          Consumers are losing money by not using the optimal card at checkout.
+          We solve it.
         </Typography>
       </MuiContainer>
     </Container>
-  )
-}
+  );
+};
 
 export default SolvingProblemsSection;
