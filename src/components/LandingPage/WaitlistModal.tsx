980c3601a4bebd7a71c77e55b9bf8ab7d6f24b26
ThaddG <thaddious.gorges@gmail.com>
2022-01-16 22:36:22 -0500

    more styling and textfield added

--- src/components/LandingPage/WaitlistModal.tsx
+++ src/components/LandingPage/WaitlistModal.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 
 const style = {
   position: 'absolute' as 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 450,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '2px solid #fff',
+  borderRadius: '28px',
   boxShadow: 24,
   p: 4,
 };
@@ -29,7 +30,19 @@ const WaitlistModal: React.FC<Props> = ({ open, setOpen }) => {
       aria-describedby="modal-for-joining-waitlist"
     >
       <Box sx={style}>
-        <Typography>bing bong</Typography>
+        <Typography variant="h2" sx={{ mb: 2 }}>
+          Join the Waitlist!
+        </Typography>
+        <Typography variant="body1" sx={{ mb: 2 }}>
+          Enter your email below to join in the waiting list and be notified
+          when we're ready for launch.
+        </Typography>
+        <TextField
+          id="email"
+          label="Email"
+          variant="standard"
+          fullWidth
+        />
       </Box>
     </Modal>
   );
