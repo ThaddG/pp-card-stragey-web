88c445359d301fe2414182cd96fda9c18d8c0c65
ThaddG <thaddious.gorges@gmail.com>
2022-01-20 00:59:09 -0500

    smoother animation and exiting animation

--- src/components/LandingPage/WaitlistModal.tsx
+++ src/components/LandingPage/WaitlistModal.tsx
@@ -13,7 +13,7 @@ import Button from '@mui/material/Button';
 import CloseIcon from '@mui/icons-material/Close';
 import Alert, { AlertColor } from '@mui/material/Alert';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const style = {
   maxWidth: 450,
@@ -23,6 +23,19 @@ const style = {
   boxShadow: 24,
   p: 4,
   m: 1,
+};
+
+const modal = {
+  hidden: {
+    y: '-100vh',
+    opacity: 0,
+    transition: { duration: 0.25, ease: 'easeIn' },
+  },
+  visible: {
+    y: '1px',
+    opacity: 1,
+    transition: { delay: 0.1, duration: 0.2, ease: 'easeOut' },
+  },
 };
 
 interface Props {
@@ -82,68 +95,78 @@ const WaitlistModal: React.FC<Props> = ({ open, setOpen }) => {
   };
 
   return (
-    <Modal
-      open={open}
-      onClose={handleOnClose}
-      aria-labelledby="join-waitlist-modal"
-      aria-describedby="modal-for-joining-waitlist"
-      sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Box
-        sx={style}
-        component={motion.div}
-        animate={{ y: '10%' }}
-        transition={{ ease: 'easeOut', duration: 0.3 }}
-      >
-        <CloseIcon
-          sx={{ cursor: 'pointer', float: 'right', textAlign: 'right' }}
-          onClick={handleOnClose}
-        />
-        <Typography variant="h2" sx={{ mb: 2 }}>
-          Join the Watchlist!
-        </Typography>
-        <Typography variant="body1" sx={{ mb: 2 }}>
-          Enter your email below to join in the waiting list and be notified
-          when we're ready for launch.
-        </Typography>
-        <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: 1 }}>
-          <TextField
-            fullWidth
-            type="email"
-            id="input-email"
-            label="Email"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <MailOutlineIcon />
-                </InputAdornment>
-              ),
-            }}
-            variant="standard"
-            value={email}
-            onChange={onChangeEmail}
-          />
-          <Button
-            variant="contained"
-            onClick={addToWatchlist}
-            sx={{ bgcolor: '#27AE60' }}
+    <AnimatePresence exitBeforeEnter>
+      {open && (
+        <Modal
+          open={open}
+          onClose={handleOnClose}
+          aria-labelledby="join-waitlist-modal"
+          aria-describedby="modal-for-joining-waitlist"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Box
+            sx={style}
+            component={motion.div}
+            variants={modal}
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
           >
-            Join!
-          </Button>
-        </Box>
-        {feedbackOpen && (
-          <Alert
-            sx={{ mt: 2 }}
-            iconMapping={{
-              success: <CheckCircleOutlineIcon fontSize="inherit" />,
-            }}
-            severity={feedbackMessage.severity}
-          >
-            {feedbackMessage.text}
-          </Alert>
-        )}
-      </Box>
-    </Modal>
+            <CloseIcon
+              sx={{ cursor: 'pointer', float: 'right', textAlign: 'right' }}
+              onClick={handleOnClose}
+            />
+            <Typography variant="h2" sx={{ mb: 2 }}>
+              Join the Watchlist!
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 2 }}>
+              Enter your email below to join in the waiting list and be notified
+              when we're ready for launch.
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: 1 }}>
+              <TextField
+                fullWidth
+                type="email"
+                id="input-email"
+                label="Email"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <MailOutlineIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="standard"
+                value={email}
+                onChange={onChangeEmail}
+              />
+              <Button
+                variant="contained"
+                onClick={addToWatchlist}
+                sx={{ bgcolor: '#27AE60' }}
+              >
+                Join!
+              </Button>
+            </Box>
+            {feedbackOpen && (
+              <Alert
+                sx={{ mt: 2 }}
+                iconMapping={{
+                  success: <CheckCircleOutlineIcon fontSize="inherit" />,
+                }}
+                severity={feedbackMessage.severity}
+              >
+                {feedbackMessage.text}
+              </Alert>
+            )}
+          </Box>
+        </Modal>
+      )}
+    </AnimatePresence>
   );
 };
 
